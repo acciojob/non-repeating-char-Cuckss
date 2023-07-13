d2ad9892431af213724605f2ff2718f42606fea2
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-13 12:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-//your JS code here. If required.
-window.alert("ho")
+function firstNonRepeatingCharacter(string) {
+  // Create an empty object to store character frequencies
+  const charFrequencies = {};
+
+  // Iterate over each character in the string and count their frequencies
+  for (let char of string) {
+    if (charFrequencies[char]) {
+      charFrequencies[char]++;
+    } else {
+      charFrequencies[char] = 1;
+    }
+  }
+
+  // Iterate over the string again to find the first non-repeating character
+  for (let char of string) {
+    if (charFrequencies[char] === 1) {
+      return char;
+    }
+  }
+
+  // If no non-repeating character is found, return null
+  return null;
+}
+
+// Prompt the user to enter a string
+const input = prompt("Enter a string:");
+
+// Call the function with the user's input
+const result = firstNonRepeatingCharacter(input);
+
+// Display the result using an alert
+alert(`The first non-repeating character is: ${result}`);
